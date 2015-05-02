39253bcadbefcefa00702a54c92a9fdc30dfe664
Jonas Konrad <me@yawk.at>
2015-05-02 22:23:59 +0200

    Use https clone

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 echo "Cloning..."
 mkdir -p ~/.config
 cd ~/.config
-git clone git@github.com:yawkat/ysh.git
+git clone https://github.com/yawkat/ysh.git
 
 echo "source ~/.config/ysh/ysh.sh" >> ~/.zshrc
 
