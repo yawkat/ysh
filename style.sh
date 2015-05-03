3a767d654e210d7db30826cd56020a8d7cf001e8
Jonas Konrad <me@yawk.at>
2015-05-03 14:57:56 +0200

    nicer terminal titles

--- style.sh
+++ style.sh
@@ -4,21 +4,45 @@ if fortune 2> /dev/null; then
     echo ""
 fi
 
+LAST_RETURN_VALUE=0
+
+# terminal title
+
+_set_title() {
+    print -Pn "\e]0;$1\a"
+}
+
+precmd() {
+    LAST_RETURN_VALUE=$?
+    chpwd
+}
+
+preexec() {
+    _set_title "$1"
+}
+
+chpwd() {
+    title=''
+    if ! [ $LAST_RETURN_VALUE = 0 ]; then
+        title+="[$LAST_RETURN_VALUE] "
+    fi
+    title+='%~'
+    _set_title $title
+}
+
 ## print command
 _pr() {
     echo -n $1
 }
 
 _build_prompt() {
-    RET=$?
-
     _pr '%F{green}%K{black}'
-    _pr '%B%n@%m%b%K{black} %T ' # hostname
+    _pr '%B%n@%m%b%K{black} ' # hostname
 
-    if ! [ $RET = 0 ]; then
+    if ! [ $LAST_RETURN_VALUE = 0 ]; then
         _pr "%F{red}"
     fi
-    printf "%3d" $RET
+    printf "%3d" $LAST_RETURN_VALUE
     _pr '%f%K{black} %F{green}%B»%b%K{black} '
 
     _pr '%~' # cwd
@@ -39,9 +63,6 @@ _build_prompt() {
 
     _pr '%k%F{green}%# %f'
 }
-
-_prompt_and_resched() { sched +10 _prompt_and_resched; zle && zle reset-prompt }
-_prompt_and_resched
 
 setopt PROMPT_SUBST
 PROMPT='$(_build_prompt)'
